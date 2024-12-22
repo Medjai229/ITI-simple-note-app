c8152aa5ff28495cc2493c6c2f372ffb19ca30a4
Malik <malikhussein229@gmail.com>
2024-12-22 21:01:50 +0200

    Adding validation to the title and description

--- addNote.js
+++ addNote.js
@@ -2,10 +2,26 @@ var addBtn = document.getElementById('add');
 var title = document.getElementById('title');
 var desc = document.getElementById('desc');
 var date = document.getElementById('date');
+var errorMsg = document.querySelectorAll('span');
 
 addBtn.addEventListener('click', (e) => {
   e.preventDefault();
   var xhr = new XMLHttpRequest();
+
+  var titleRegex = /^[a-zA-Z]{6,}/;
+  var descRegex = /^.{20,}/;
+
+  if (!titleRegex.test(title.value)) {
+    errorMsg[0].style.opacity = 1;
+    return;
+  }
+  errorMsg[0].style.opacity = 0;
+
+  if (!descRegex.test(desc.value)) {
+    errorMsg[1].style.opacity = 1;
+    return;
+  }
+  errorMsg[1].style.opacity = 0;
 
   var data = JSON.stringify({
     title: title.value,
--- editNote.js
+++ editNote.js
@@ -3,6 +3,7 @@ var title = document.getElementById('title');
 var desc = document.getElementById('desc');
 var date = document.getElementById('date');
 var noteData = JSON.parse(sessionStorage.getItem('noteData'));
+var errorMsg = document.querySelectorAll('span');
 title.value = noteData.title;
 desc.value = noteData.description;
 date.value = noteData.createdAt;
@@ -10,6 +11,21 @@ date.value = noteData.createdAt;
 saveBtn.addEventListener('click', (e) => {
   e.preventDefault();
   var xhr = new XMLHttpRequest();
+
+  var titleRegex = /^[a-zA-Z]{6,}/;
+  var descRegex = /^.{20,}/;
+
+  if (!titleRegex.test(title.value)) {
+    errorMsg[0].style.opacity = 1;
+    return;
+  }
+  errorMsg[0].style.opacity = 0;
+
+  if (!descRegex.test(desc.value)) {
+    errorMsg[1].style.opacity = 1;
+    return;
+  }
+  errorMsg[1].style.opacity = 0;
 
   var data = JSON.stringify({
     title: title.value,
